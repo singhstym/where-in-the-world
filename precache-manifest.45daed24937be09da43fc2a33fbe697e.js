5b5798a3a3d9cb948fcf3dd4c84c78c02a75539d
Satyam Singh <stymsingh@outlook.com>
2020-06-16 22:11:25 +0530

    Updates

---
+++ precache-manifest.45daed24937be09da43fc2a33fbe697e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa03d79a7f3c07f1e220b14b51f1b7f",
+    "url": "/where-in-the-world/index.html"
+  },
+  {
+    "revision": "5da362b00d793ff9a34e",
+    "url": "/where-in-the-world/static/css/main.af498845.chunk.css"
+  },
+  {
+    "revision": "944ece1045a6d61a83b4",
+    "url": "/where-in-the-world/static/js/2.477853d5.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/where-in-the-world/static/js/2.477853d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5da362b00d793ff9a34e",
+    "url": "/where-in-the-world/static/js/main.e0796ddf.chunk.js"
+  },
+  {
+    "revision": "8c34ddd56247d26c7a93",
+    "url": "/where-in-the-world/static/js/runtime-main.b9ecdf46.js"
+  }
+]);
